7ec17553b735d51a1f5c7da2f84e4cb88964824f
github.com/munjunoh02 <munjunoh02@naver.com>
2016-12-31 16:59:36 +0900

    mod

--- NHN_PreTask/src/main/java/com/spring/nhn/dao/NHNDao.java
+++
@@ -1,7 +0,0 @@
-package com.spring.nhn.dao;
-import java.util.List;
-import com.spring.nhn.model.NHNModel;
-
-public interface NHNDao {
-	List<NHNModel> getBookList();
-}
--- NHN_PreTask/src/main/java/com/spring/nhn/service/NHNService.java
+++
@@ -1,7 +0,0 @@
-package com.spring.nhn.service;
-import java.util.List;
-import com.spring.nhn.model.NHNModel;
-
-public interface NHNService {
-	List<NHNModel> getBookList();
-}
